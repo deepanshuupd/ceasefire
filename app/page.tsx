d0c89af7fa34bbdeed933a7986a7483cec06df0d
SahilLamba@2001 <19bcs1922@gmail.com>
2026-04-13 12:44:01 +0530

    fixed shadcn and heroSection

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { HeroClipsSection } from "@/components/HeroSection";
+
+export default function Home() {
+	return (
+		<div className="flex flex-col flex-1 items-center justify-center bg-zinc-900 font-sans dark:bg-black">
+			<HeroClipsSection />
+		</div>
+	);
+}
